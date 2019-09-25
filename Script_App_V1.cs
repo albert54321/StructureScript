e6147eb893e7125e2284fd3b6e9fd1584de2fe37
Alberto Alarcon <alarcon.alberto_01@hotmail.com>
2019-09-25 11:00:52 -0300

    se anade el ptv al sib

--- Script_App_V1.cs
+++ Script_App_V1.cs
@@ -24,8 +24,8 @@ using System.Windows.Forms;
 
 // TODO: uncomment the line below if the script requires write access.
 [assembly: ESAPIScript(IsWriteable = true)]
-[assembly: AssemblyVersion("1.0.0.184")]
-[assembly: AssemblyFileVersion("1.0.0.184")]
+[assembly: AssemblyVersion("1.0.0.186")]
+[assembly: AssemblyFileVersion("1.0.0.186")]
 [assembly: AssemblyInformationalVersion("1.0")]
 
 
